73b87877ffec9861bd8bd1ec83251768d0253694
Corbin Charpentier <corbin.charpentier@gmail.com>
2022-02-21 15:26:14 -0800

    Add pre-commit hook linter

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,11 @@
 from dplypy import DplyFrame
 
+
 def query(query_str: str):
     return lambda d1: DplyFrame(d1.pandas_df.query(query_str))
 
-def apply(func, axis=0, **kwargs): # TODO: CSC: use kwargs... couldn't quickly figure out how to do it
-    return lambda d1: DplyFrame(d1.pandas_df.apply(func=func, axis=axis)) 
+
+def apply(
+    func, axis=0, **kwargs
+):  # TODO: CSC: use kwargs... couldn't quickly figure out how to do it
+    return lambda d1: DplyFrame(d1.pandas_df.apply(func=func, axis=axis))
--- test/pipeline_test.py
+++ test/pipeline_test.py
@@ -3,14 +3,18 @@ import numpy as np
 from dplypy import DplyFrame
 from pipeline import query, apply
 
-def _test(): # TODO: convert to pytest 
-    pandas_df = pd.DataFrame(np.array(([1, 2, 3], [1, 5, 6], [6, 7, 8])),
-                          index=['mouse', 'rabbit', 'owl'],
-                          columns=['col1', 'col2', 'col3'])
+
+def _test():  # TODO: convert to pytest
+    pandas_df = pd.DataFrame(
+        np.array(([1, 2, 3], [1, 5, 6], [6, 7, 8])),
+        index=["mouse", "rabbit", "owl"],
+        columns=["col1", "col2", "col3"],
+    )
 
     d = DplyFrame(pandas_df)
-    output = d + apply(lambda x: x + 1) + query('col1 == 2')
+    output = d + apply(lambda x: x + 1) + query("col1 == 2")
     print(output.pandas_df)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     _test()
