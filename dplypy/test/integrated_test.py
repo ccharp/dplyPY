bf6a2acd54f8447ee819ac213ac2c3d9fcf7d9e4
Matthew Chau <26354088+TOLOTOLO999@users.noreply.github.com>
2022-03-09 21:02:57 -0800

    Migrated integrated tests from example.ipynb

--- dplypy/test/integrated_test.py
+++ dplypy/test/integrated_test.py
@@ -32,6 +32,25 @@ def test_pipeline():
     expected_2 = pd.get_dummies(test_df_1.pandas_df, columns=["embarked"])
     pd.testing.assert_frame_equal(output_2.pandas_df, expected_2)
 
+    # drop + fill_na + count_null
+    output_3 = (
+        test_df_1
+        + drop(labels=["survived", "pclass"], axis=1)
+        + fill_na(value={"deck": "A"})
+        + count_null("deck")
+    )
+    expected_3 = 0
+    assert output_3 == expected_3
+
+    # query + apply
+    output_4 = test_df_3 + select("age > 25") + mutate(lambda b: b + 100)
+    expected_4 = test_df_3.pandas_df.query('age > 25').apply(lambda b: b + 100)
+    pd.testing.assert_frame_equal(output_4.pandas_df, expected_4)
+
+    # merge + drop_na
+    output_5 = test_df_3 + merge(test_df_4, on="pclass", how="outer") + drop_na()
+    expected_5 = test_df_3.pandas_df.merge(test_df_4.pandas_df, on='pclass', how='outer').dropna()
+    pd.testing.assert_frame_equal(output_5.pandas_df, expected_5)
 
 
 if __name__ == '__main__':
